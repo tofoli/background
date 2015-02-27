23c6e47af1ac0f938006c7602476120967092163
Luis <tofoli@unochapeco.edu.br>
2015-02-27 17:01:38 -0300

    Create background.rb

---
+++ background.rb
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+@@queue = []
+class Background
+  def initialize
+	  th = Thread.new do
+			while @@queue.index(th) != 0
+				sleep 10
+			end
+
+			begin
+				yield
+			rescue Exception => e
+				@error = e
+			ensure
+				@@queue.delete(th)
+				@block.call if @block
+			end
+		end
+
+		@@queue << th
+	end
+
+	def finished(*arg, &block)
+		@block = block
+	end
+
+	def error?
+		!@error.nil?
+	end
+
+	def error
+		@error
+	end
+end
