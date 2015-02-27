54593e6525b1819cf4952b2f6e26782c1983fc29
Luis <tofoli@unochapeco.edu.br>
2015-02-27 17:08:13 -0300

    Update background.rb

--- background.rb
+++ background.rb
@@ -2,34 +2,34 @@
 
 @@queue = []
 class Background
-	def initialize
-		th = Thread.new do
-			while @@queue.index(th) != 0
-				sleep 10
-			end
+    def initialize
+        th = Thread.new do
+            while @@queue.index(th) != 0
+                sleep 10
+            end
 
-			begin
-				yield
-			rescue Exception => e
-				@error = e
-			ensure
-				@@queue.delete(th)
-				@block.call if @block
-			end
-		end
+            begin
+                yield
+            rescue Exception => e
+                @error = e
+            ensure
+                @@queue.delete(th)
+                @block.call if @block
+            end
+        end
 
-		@@queue << th
-	end
+        @@queue << th
+    end
 
-	def finished(*arg, &block)
-		@block = block
-	end
-
-	def error?
-		!@error.nil?
-	end
-
-	def error
-		@error
-	end
+    def finished(*arg, &block)
+        @block = block
+    end
+    
+    def error?
+        !@error.nil?
+    end
+    
+    def error
+        @error
+    end
 end
